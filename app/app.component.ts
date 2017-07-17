31d271315b06b7d772604d1382aa02aff77add8a
Michael Koceja <michael.koceja@gmail.com>
2017-07-17 15:52:10 -0700

    add edit form component and ability to hide form when edit is complete

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Task } from './task.model'
+import { Task } from './task.model';
 
 @Component({
   selector: 'app-root',
@@ -7,24 +7,9 @@ import { Task } from './task.model'
     <div class="container">
       <h1>To Do List for {{month}}/{{day}}/{{year}}</h1>
       <h3>{{currentFocus}}</h3>
-
       <task-list [childTaskList]="masterTaskList" (clickSender)="editTask($event)"></task-list>
       <hr>
-      <div>
-        <div *ngIf="selectedTask">
-          <h3>{{selectedTask.description}}</h3>
-          <p>Task Complete? {{selectedTask.done}}</p>
-          <hr>
-          <h3>Edit Task</h3>
-          <label>Enter Task Description:</label>
-          <input [(ngModel)]="selectedTask.description">
-          <label>Enter Task Priority (1-3):</label><br>
-          <input type="radio" [(ngModel)]="selectedTask.priority" [value]="1">1 (Low Priority)<br>
-          <input type="radio" [(ngModel)]="selectedTask.priority" [value]="2">2 (Medium Priority)<br>
-          <input type="radio" [(ngModel)]="selectedTask.priority" [value]="3">3 (High Priority)
-          <button (click)="finishedEditing()">Done</button>
-        </div>
-      </div>
+      <edit-task [childSelectedTask]="selectedTask" (doneButtonClickedSender)="finishedEditing()"></edit-task>
     </div>
   `
 })
@@ -50,4 +35,5 @@ export class AppComponent {
   finishedEditing() {
     this.selectedTask = null;
   }
+
 }
